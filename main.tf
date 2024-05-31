99292b261679dfe2aad2b8aa22da2c97985502c6
Brainace77 <168272097+Brainace77@users.noreply.github.com>
2024-05-31 16:48:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "RM" {
   ami = "ami-00fa32593b478ad6e"
-  key_name = "terra-key1"
-  instance_type = "t2.medium"
+  key_name = "new-jenkins"
+  instance_type = "t2.micro"
   #defrgt
   tags = {
     Name = "terraform"
